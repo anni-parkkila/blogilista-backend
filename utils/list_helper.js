fcfda7473a730db7bc101d808e451701529547d8
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-13 17:36:21 +0300

    step 2 totalLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (acc, cur) => acc + cur.likes,
+    0)
+}
+
 const dummy = (blogs) => {
   return 1
 }
 
 module.exports = {
+  totalLikes,
   dummy
 }
