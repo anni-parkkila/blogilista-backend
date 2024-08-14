3302294945310d3068718be565dc8e3c6a42be64
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-14 13:12:48 +0300

    step3 favoriteBlog

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -2,9 +2,11 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,11 +4,28 @@ const totalLikes = (blogs) => {
     0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  } else {
+    blogs.sort((a, b) => a.likes - b.likes)
+    const favorite =
+        {
+          title: blogs.at(-1).title,
+          author: blogs.at(-1).author,
+          likes: blogs.at(-1).likes
+        }
+    return favorite
+  }
+}
+
 const dummy = (blogs) => {
+  console.log(blogs)
   return 1
 }
 
 module.exports = {
   totalLikes,
+  favoriteBlog,
   dummy
 }
