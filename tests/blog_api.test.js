056aa5d0140d5eb30b97dc44f0858197e2850bf6
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-15 16:52:37 +0300

    beforeEach-optimointi

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -10,10 +10,7 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(helper.initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(helper.initialBlogs[1])
-  await blogObject.save()
+  await Blog.insertMany(helper.initialBlogs)
 })
 
 test('blogs are returned as json', async () => {
