c8de2de139b677f1e5653d0b7e5ed5bbe1818e23
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-15 13:07:12 +0300

    step 1 HTTP GET

---
+++ tests/blog_api.test.js
@@ -0,0 +1,47 @@
+const { test, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+const app = require('../app')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Matin Testiploki',
+    author: 'Matti Meikäläinen',
+    url: 'matinploki.fi',
+    likes: 1
+  },
+  {
+    title: 'Maijan Testiblogi',
+    author: 'Maija Mehiläinen',
+    url: 'maijanblogi.fi',
+    likes: 3
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
