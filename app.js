b44f6b95dea4308c045a1b70348444bac25d8a28
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-21 19:02:20 +0300

    Step8

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
